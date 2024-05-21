17686a7c7085f27aa56ce298f57b696c6fcf896c
Benja Aguilar <benjaaguilar02@gmail.com>
2024-05-21 17:49:44 -0300

    created distr file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
 import css from './styles.css';
+import benja from './imgs/benja.jpg';
+import benja2 from './imgs/benja-kite.jpg';
+
+document.querySelector('.benja').src = benja;
+document.querySelector('.img-contact').src = benja2;
